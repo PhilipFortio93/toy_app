711ea6c95363a00e295eec233131c7995cd35b58
PhilipFortio93 <philip.fortio@gmail.com>
2017-12-30 18:27:51 +0000

    fixed mixed up root with controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  root 'application#hello'
+  def hello
+  	render html: "hello, world, this is the beginning!"
+  end
+
 end
